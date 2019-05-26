cf6bc680ace801fa6474850ba56731a7a9a2e0e2
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-26 15:37:47 +0200

    added new template react project of note app

---
+++ src/Note.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+import { FaPencilAlt } from 'react-icons/fa'
+import { FaTrash } from 'react-icons/fa'
+
+class Note extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            editing: false
+        }
+        this.edit = this.edit.bind(this)
+        this.remove = this.remove.bind(this)
+    }
+
+    edit() {
+        alert('editing note')
+    }
+    remove() {
+        alert('removing note')
+    }
+
+
+    render() {
+        return (
+            <React.Fragment>
+                <div className="note">
+                    <p>Learn React</p>
+                    <span>
+                        <button onClick={this.edit} id="edit" ><FaPencilAlt />></button>
+                        <button onClick={this.remove} id="remove"><FaTrash /></button>
+                    </span>
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Note;
