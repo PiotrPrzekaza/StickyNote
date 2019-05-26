e12868c1d9832066f8be561e5edbe38dd005c1a6
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-26 19:23:46 +0200

    addet to note app , displaying static note and fix error with <> wrap components

--- src/Note.js
+++ src/Note.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { FaPencilAlt } from 'react-icons/fa'
-import { FaTrash } from 'react-icons/fa'
+
+import FaPencil from 'react-icons/lib/fa/pencil'
+import FaTrash from 'react-icons/lib/fa/trash'
+import FaFloppy from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
     constructor(props) {
@@ -10,28 +12,57 @@ class Note extends Component {
         }
         this.edit = this.edit.bind(this)
         this.remove = this.remove.bind(this)
+        this.renderForm = this.renderForm.bind(this)
+        this.renderDisplay = this.renderDisplay.bind(this)
     }
 
     edit() {
-        alert('editing note')
+        this.setState({
+            editing: true
+        })
     }
     remove() {
         alert('removing note')
     }
+    save() {
+        alert(this._newText.value)
+    }
+
+    renderForm() {
+        return (
+            <>
+                <div className="note">
+                    <form>
+                        <textarea ref={input => this._newText = input} />
+                        <button><FaFloppy /></button>
+                    </form>
+                </div>
+            </>
+        )
+    }
 
 
-    render() {
+    renderDisplay() {
         return (
             <React.Fragment>
                 <div className="note">
-                    <p>Learn React</p>
+                    <p>{this.props.children}</p>
                     <span>
-                        <button onClick={this.edit} id="edit" ><FaPencilAlt />></button>
+                        <button onClick={this.edit} id="edit" ><FaPencil />></button>
                         <button onClick={this.remove} id="remove"><FaTrash /></button>
                     </span>
                 </div>
             </React.Fragment>
         )
+    }
+    render() {
+        return this.state.editing ? this.renderForm() : this.renderDisplay()
+
+        // if (this.state.editing) {
+        //     return this.renderForm();
+        // } else {
+        //     return this.renderDisplay();
+        // }
     }
 }
 
