7a160ed4dbd8688e74e39eef7a689d2594d5a809
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-27 19:34:06 +0200

    Updates

---
+++ precache-manifest.e927bc559254c4edd1dfff4c01504f08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b079414aeefd0a02a89f469f7d011a",
+    "url": "/StickyNote/index.html"
+  },
+  {
+    "revision": "ab023ad68ed8066b58be",
+    "url": "/StickyNote/static/css/main.11c48fb9.chunk.css"
+  },
+  {
+    "revision": "ec666c2ebdc1b1a9c926",
+    "url": "/StickyNote/static/js/2.0fe81a13.chunk.js"
+  },
+  {
+    "revision": "ab023ad68ed8066b58be",
+    "url": "/StickyNote/static/js/main.2a2b822e.chunk.js"
+  },
+  {
+    "revision": "0443a30189d4c1c4cd5a",
+    "url": "/StickyNote/static/js/runtime~main.41e557a9.js"
+  }
+]);
