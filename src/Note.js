46703af34b6f551456cb4fcb15c93858c37c1663
Piotr Przekaza <piotr.przekaza@hotmail.com>
2019-05-27 14:54:41 +0200

    added update method to update notes in app

--- src/Note.js
+++ src/Note.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import FaPencil from 'react-icons/lib/fa/pencil'
 import FaTrash from 'react-icons/lib/fa/trash'
-import FaFloppy from 'react-icons/lib/fa/floppy-o'
+import FaFloppyO from 'react-icons/lib/fa/floppy-o'
 
 class Note extends Component {
     constructor(props) {
@@ -14,6 +14,7 @@ class Note extends Component {
         this.remove = this.remove.bind(this)
         this.renderForm = this.renderForm.bind(this)
         this.renderDisplay = this.renderDisplay.bind(this)
+        this.save = this.save.bind(this)
     }
 
     edit() {
@@ -24,17 +25,21 @@ class Note extends Component {
     remove() {
         alert('removing note')
     }
-    save() {
-        alert(this._newText.value)
+    save(e) {
+        e.preventDefault()
+        this.props.onChange(this._newText.value, this.props.index)
+        this.setState({
+            editing: false
+        })
     }
 
     renderForm() {
         return (
             <>
                 <div className="note">
-                    <form>
+                    <form onSubmit={this.save}>
                         <textarea ref={input => this._newText = input} />
-                        <button><FaFloppy /></button>
+                        <button id="save"><FaFloppyO /></button>
                     </form>
                 </div>
             </>
